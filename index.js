4eda52c718ecb9376fb7688d8ad83549c3a2ecaa
sugitomone <127827194+sugitomone@users.noreply.github.com>
2023-03-14 16:13:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 window.onload = function () {
 
-    const list = {
-        "daikichi":10,
-        "kichi":15,
-        "chuukichi":20,
-        "syoukichi":25,
-        "suekichi":15,
-        "kyou":10,
-        "daikyou":5,
-    };
+const list = {
+    "daikichi":10,
+    "kichi":15,
+    "chuukichi":20,
+    "syoukichi":25,
+    "suekichi":15,
+    "kyou":10,
+    "daikyou":5,
+};
 
-    var timer;
-    // START・STOPボタンクリックのイベント
-    document.getElementById("button").onclick = function () {
-        // START
-        if (this.textContent == "START") {
-            // ボタンのテキストを"STOP"に変更
-            this.textContent = "STOP";
+var timer;
+// START・STOPボタンクリックのイベント
+document.getElementById("button").onclick = function () {
+// START
+if (this.textContent == "START") {
+    // ボタンのテキストを"STOP"に変更
+    this.textContent = "STOP";
 
-            // ルーレットタイマーを設定
-            timer = setInterval(function () {
-                var random = Math.floor(Math.random() * Object.keys(list).length);
-                document.getElementById("img").src = Object.keys(list)[random] + ".png";
-            }, 10);
-        // STOP
-        } else {
-            // ボタンのテキストを"START"に変更
-            this.textContent = "START";
+    // ルーレットタイマーを設定
+    timer = setInterval(function () {
+        var random = Math.floor(Math.random() * Object.keys(list).length);
+        document.getElementById("img").src = Object.keys(list)[random] + ".png";
+    }, 10);
+// STOP
+} else {
+    // ボタンのテキストを"START"に変更
+    this.textContent = "START";
 
-            var rate = 0;
-            var random = Math.floor(Math.random() * 100);
-            for(var data in list){
-                rate += list[data];
-                if(random <= rate){
-                    document.getElementById("img").src = data + ".png";
+    var rate = 0;
+    var random = Math.floor(Math.random() * 100);
+    for(var data in list){
+        rate += list[data];
+        if(random <= rate){
+            document.getElementById("img").src = data + ".png";
 
-                    // ルーレットタイマーを停止
-                    clearInterval(timer);
-                    return;
-                }
-            }
+            // ルーレットタイマーを停止
+            clearInterval(timer);
+            return;
+        }
+    }
 
